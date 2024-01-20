73efcfe449964540251725c90edb2a03e7e9e1fe
Eduardo5011 <eduardo.rosado5011@gmail.com>
2024-01-20 18:54:30 -0500

    updated trends

--- src/components/TrendingGames.jsx
+++ src/components/TrendingGames.jsx
@@ -1,19 +1,30 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
 
-const TrendingGames = ({gameList}) => {
-
-    useEffect(() => {
-        console.log(gameList)
-    })
+const TrendingGames = ({ gameList }) => {
+  useEffect(() => {
+    console.log(gameList);
+  });
   return (
-    <div className="grid grid-cols-4">
-        {gameList.map((item, index) => index < 4 &&(
-            <div>
-                <img src={item.background_image} alt="" />
-                <h2>{item.name}</h2>
-            </div>
-        ))}
+    <div className="mt-5 hidden md:block">
+      <h2 className="font-bold text-[30px] dark:text-white">Trending Games</h2>
+      <div className=" md:grid md:grid-cols-3 gap-4 mt-5  lg:grid-cols-4">
+        {gameList.map(
+          (item, index) =>
+            index < 4 && (
+              <div className="bg-[#76a8f75e] rounded-lg group hover:scale-110 transition-all duration-300 ease-in-out cursor-pointer" key={item.id || index}>
+                <img
+                  className="h-[270px] rounded-t-lg object-cover"
+                  src={item.background_image}
+                  alt={item.name || "Game Image"}
+                />
+                <h2 className="dark:text-white p-2 text-[20px] font-bold">
+                  {item.name}
+                </h2>
+              </div>
+            )
+        )}
+      </div>
     </div>
-  )
-}
-export default TrendingGames
+  );
+};
+export default TrendingGames;
